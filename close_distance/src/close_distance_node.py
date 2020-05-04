9eafc476ce63b98dd5295f5d2724eb65da8d313b
Mkolb <monica_kolb@student.uml.edu>
2020-05-04 05:12:50 -0700

    Added a 'close enough' range. Removed hooks for controlling angle.

--- close_distance/src/close_distance_node.py
+++ close_distance/src/close_distance_node.py
@@ -20,28 +20,19 @@ class PID:
 		# Subscribe to the sign information that gets me distance and angle.
 		rospy.Subscriber('/sign_info',SignInfo,self.adjust_motion)
 
-		# MUST include a velocity and angle, like the Twist2DStamped.
+		# Must include a velocity and angle, like the Twist2DStamped.
 		self.pub = rospy.Publisher('horriblegoose/car_cmd_switch_node/cmd', Twist2DStamped, queue_size=10)
 
-#		self.orien=Twist2DStamped
-#		self.orien.v=0
-#		self.orien.omega=0
-
-		# Initialize my unit's location.
 		# Initialize timing and measurements.
 		self.previous_time = time.time()
 		self.current_time=self.previous_time+0.001
 		self.elapsed_time = self.current_time - self.previous_time
 		self.last_measurement=0
 		self.current_measurement=0
-		# Distance controller
+		# Distance controller - comes from dist_to_sign
 		self.dp = 0
 		self.di = 0
-		self.dd = 0
-		# Angle controller
-		self.ap = 0
-		self.ai = 0
-		self.ad = 0
+		self.dd=0
 
 
 
@@ -59,24 +50,26 @@ class PID:
 			current_measurement = data.dist_to_sign-0.2
 			self.current_measurement=current_measurement
 
+			# This will only be published if current_measurement<0.01
 			kdp=1
 			kdi=0
-			kdd=0
+			kdd=1
 
-			adp=0
-			adi=0
-			add=0
 			self.dp = current_measurement #data
 			self.di = 0 
 			self.dd = 0 
 			
 			twist.v=(kdp*self.dp+kdi*self.di+kdd*self.dd)
-		
-			twist.omega=(adp*self.ap+adi*self.ai+add*self.ad)
+			twist.omega=0
 
-#			self.pub.publish(kdp*self.dp + kdi*self.di + kdd*self.dd,
-#				adp*self.ap+adi*self.ai+ add*self.ad) #straight line, angle
-			self.pub.publish(twist)
+
+			if current_measurement>0.01:
+				self.pub.publish(twist)
+
+		#else: # If no tag was detected
+		#	twist.v=0
+		#	twist.omega=0
+		#	self.pub.publish(twist)
 
 if __name__ == '__main__':
 	# Get that node started - the launch file will use this
