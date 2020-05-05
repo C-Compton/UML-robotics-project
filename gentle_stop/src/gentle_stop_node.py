55d78492f2f9f3e76d491b52406716a6b446a1ab
root <root@horriblegoose.localdomain>
2020-05-05 20:29:58 +0000

    Debugging. Why does no publish statement work?

--- gentle_stop/src/gentle_stop_node.py
+++ gentle_stop/src/gentle_stop_node.py
@@ -4,10 +4,10 @@ import rospy
 import time
 from duckietown_msgs.msg import LanePose, Twist2DStamped
 from sign_reader.msg import SignInfo
-
+from std_msgs.msg import Float32
 
 _STOP_DISTANCE=0.22 # the width of one lane
-_THRESHOLD=0.05
+_THRESHOLD=0.01
 
 class PidController:
 
@@ -35,11 +35,16 @@ class Stopper:
     
     def __init__(self):
         # Intercept the original message
-        rospy.Subscriber('lane_controller_node/car_cmd', Twist2DStamped, self.checkCarCmd)
+        rospy.Subscriber('horriblegoose/lane_controller_node/car_cmd', Twist2DStamped, self.checkCarCmd)
         rospy.Subscriber('sign_reader_node/sign_info', SignInfo, self.checkSign)
         self.pub = rospy.Publisher('stop_controller_node/car_cmd', Twist2DStamped, queue_size=10)
+        self.debug=rospy.Publisher('pid_output',Float32,queue_size=10)
         self.speed=0 # Twist2DStamped d
         self.heading=0 # Twist2DStamped omega
+        self.kd=1
+        self.ki=0
+        self.kd=0
+        self.ctrl=PidController(self.kd,self.ki,self.kd,0,0)
 
     def checkCarCmd(self,carCmd_baseline):
         if self.did_see_sign == False: 
@@ -50,23 +55,28 @@ class Stopper:
     def checkSign(self, sign_msg):
         sign = sign_msg.sign
         self.dist=sign_msg.dist_to_sign
-        if sign is 'NONE':
-            self.did_see_sign = False        
-        else:
-            self.did_see_sign = True
-            self.current_time=time()
+        if sign is 'STOP':
+
             if self.previous_time is None:
                 self.previous_time=time()
 
-	        kp=1
+            self.current_time=time()
+
+            kp=1
             ki=0
-            kd=1
+            kd=0
             init_error=self.dist
-            init_integ=0
+            if self.integ is None:
+                self.integ=0
 
             output = Twist2DStamped()
-            vel=PidController(kp, ki, kd, init_error, init_integ)
-            new_error=vel.pid(prev_time, curr_time, vel)
+            if self.count is None:
+                self.count=0
+            self.count+=1
+            self.debug.publish(self.count)
+            #vel=PidController(kp, ki, kd, init_error, self.integ)
+            ctrl_output=self.ctrl.pid(self.previous_time, self.current_time,self.dist-_STOP_DISTANCE)
+            self.debug.publish(ctrl_output)
             self.previous_time=self.current_time
 
             if self.speed>_THRESHOLD:
