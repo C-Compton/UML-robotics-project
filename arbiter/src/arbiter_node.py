856ba61c0816ac85bb0220991f3ff9e94e229501
Chris Compton <christopher.j.compton@gmail.com>
2020-05-07 23:32:35 -0400

    Don't run stop in a loop.

--- arbiter/src/arbiter_node.py
+++ arbiter/src/arbiter_node.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
 import rospy
+
 import math
+import threading
 
 from duckietown_msgs.msg import LanePose, Twist2DStamped, BoolStamped
 from sign_reader.msg import SignInfo
@@ -9,12 +11,15 @@ from utilities.utils import PidController
 from enum import Enum
 
 _NODE_NAME = 'arbiter_node'
-_LOW_SPEED=0.1
-_MED_SPEED=0.2
-_HI_SPEED=0.5
-_STOP_DISTANCE=0.6 # the width of one lane
-_SPEED_THRESHOLD=0.01
 
+_STOP_DISTANCE=0.6 # the width of one lane
+
+
+class SPEED(Enum):
+    LOW = 0.1
+    MED = 0.2
+    HI  = 0.5
+    TH  = 0.01
 
 class THRES_LEFT(Enum):
     D = 0.25
@@ -23,7 +28,7 @@ class THRES_LEFT(Enum):
 class THRES_RIGHT(Enum):
     D = 0.10
     PHI = 0.40
-    
+
 class Arbiter:
     
     def __init__(self):
@@ -37,13 +42,16 @@ class Arbiter:
 
         self.blocking = False
         self.d_phi_oor = False
-        self.did_see_sign = False
-        self.d_values = []
-        self.phi_values = []
-        self.speed=_MED_SPEED # Twist2DStamped v
+        
+        self.d_err = 0.0
+        self.phi_err = 0.0
+
+        self.speed_limit = SPEED.MED
         self.heading=0 # Twist2DStamped omega
+
         self.vMultiplier = 1
-        self.stopCtrl = PidController(0.5, 0, 0, 0, 0)
+        self.stopCtrl = PidController(0.5, 0, 0, rospy.get_time(), 0, 0)
+
         self.did_see_sign = False
         self.last_sign = 'NONE'
 
@@ -51,122 +59,90 @@ class Arbiter:
         self.curr_v = selfState.v
         self.curr_omega = selfState.omega
 
-    def checkCarCmd(self,carCmd_baseline):
-        if self.did_see_sign == False: # If there's no sign to handle, use LF
-            self.pub.publish(carCmd_baseline)
+    def publish(self, v, o):
+        output = Twist2DStamped()
+        output.v = min(v, self.speed_limit)
+        output.o = o
+        self.pub.publish(output)
+        
+    def checkCarCmd(self, car_cmd_baseline):
+        # If there's no sign to handle, use LF
+        if not self.did_see_sign:
+            v = car_cmd_baseline.v
+            o = car_cmd_baseline.omega
+            self.publish(v, o)
         else:
             self.arbitration()
 
-    def checkLanePose(self, lanePose_msg):
-        self.d_values.append(lanePose_msg.d)
-        self.phi_values.append(lanePose_msg.phi)
-
-        while len(self.d_values) >= 20 and len(self.phi_values) >= 20:
-            # TODO Probably not the most efficient way to do this but it works for now
-            self.d_values.pop(0)
-            self.phi_values.pop(0)
-
-        d_error = sum(list(map(abs, self.d_values))) / float(len(self.d_values))
-        phi_error = sum(list(map(abs, self.phi_values))) / float(len(self.phi_values))
-
-        if self.last_sign is 'LEFT':
-            self.threshold = THRES_LEFT
-        elif self.last_sign is 'RIGHT':
-            self.threshold = THRES_RIGHT
-        else:
-            self.threshold = None
-                    
-
-        if d_error > self.threshold.D or phi_error > self.threshold.PHI:
-            self.d_phi_oor = True
-        else:
-            self.d_phi_oor = False
-            self.threshold = None
-                    
-        if self.threshold is not None:
-            if d_error > self.threshold.D or phi_error > self.threshold.PHI:
-                self.d_phi_oor = True
-            else:
-                self.d_phi_oor = False
-    
     def checkSign(self, sign_msg):
-        self.sign = sign_msg.sign
+        sign = sign_msg.sign
         self.dist_to_sign = sign_msg.dist_to_sign
 
-        if self.sign is not 'NONE':
+        if sign is not 'NONE':
             self.did_see_sign = True
-            self.last_sign = self.sign
+            self.last_sign = sign
 
-        # Should this be here or in chackCarCmd? If we call it here, Jooseppi
-        # feels like it might miss wheel command messages
-        # self.arbitration()
+    
+    def checkLanePose(self, lanePose_msg):
+        self.d_err = lanePose_msg.d
+        self.phi_err = lanePose_msg.phi    
 
     def gentleStop(self):
-        output = Twist2DStamped()
-        previous_time = time.time()
-        integ = 0
+        if self.curr_v <= SPEED.TH:
+            self.publish(0.0, 0.0)
+            rospy.Timer(rospy.Duration(3), lambda self: self.did_see_sign=False)
+            return
+
         error_dist = self.dist_to_sign - _STOP_DISTANCE # calculate error
+        curr_time = rospy.get_time()
 
-        while(self.speed > _SPEED_THRESHOLD):
-            current_time = time.time()
-            ctrl_output = self.stopCtrl.pid(previous_time, current_time, error_dist)
-            previous_time = current_time
-
-            self.speed = ctrl_output
-            rospy.loginfo('speed in gentleStop: ' + str(self.speed))
-            output.v = self.speed
-            output.omega = self.heading
-
-            self.pub.publish(output)
-
-        output.v = 0
-        output.omega = 0
-        self.pub.publish(output)
+        self.stopCtrl.calibrateTime(curr_time - 0.0001)
+        
+        v  = self.stopCtrl.pid( curr_time, error_dist)
+        rospy.logwarn("gentleStop : " + str(v))
+        self.publish(v, self.curr_omega)
 
     def slowDown(self):
-        self.speed=_LOW_SPEED
+        self.speed_limit = SPEED.LOW
 
     def speedUp(self):
-        self.speed=_HI_SPEED
+        self.speed_limit = SPEED.MED
 
     def moveRobot(self, v, omega, duration_time, rate=100):
         # Generic function for moving the robot for a certain amount of time
-        output = Twist2DStamped()
         start_time = rospy.get_time()  
         while (rospy.get_time() - start_time) < duration_time:
-            output.v = v
-            output.omega = omega
             rate = rospy.Rate(rate)
-            self.pub.publish(output)
+            self.publish(v, omega)
             rate.sleep()
 
         output.v = 0
         output.omega = 0
-        self.pub.publish(output)
+        self.publish(0, 0)
 
     def turn(self):
         # Assume that the robot comes to a stop before executing a turn.
         # Use the phi value to center it
-        if self.phi_values[-1] > 0.2 or self.phi_values[-1] < -0.2:
-            self.moveRobot(0, -2 * self.phi_values[-1], 0.5)
+        if math.fabs(self.phi_err) > 0.2: 
+            self.moveRobot(0, -2 * self.phi_err, 0.5)
 
         # Then, based on the sign, we turn
         # We set more or less aggressive omegas based on the position of the 
         # robot in the lane
         if self.last_sign == 'LEFT':
-            if math.fabs(self.d_values[-1]) <= 0.05:
+            if math.fabs(self.d_err) <= 0.05:
                 moveRobot(0.25, 2.2, 3)
-            elif self.d_values[-1] > 0.05:
+            elif self.d_err > 0.05:
                 moveRobot(0.25, 1.9, 3)
-            elif self.d_values[-1] < -0.05:
+            elif self.d_err < -0.05:
                 moveRobot(0.25, 2.5, 3)
 
         elif self.last_sign == 'RIGHT':
-            if math.fabs(self.d_values[-1]) <= 0.05:
+            if math.fabs(self.d_err) <= 0.05:
                 moveRobot(0.25, -3.8, 1.5)
-            elif self.d_values[-1] > 0.05:
+            elif self.d_err > 0.05:
                 moveRobot(0.25, -3.9, 1.5)
-            elif self.d_values[-1] < -0.05:
+            elif self.d_err < -0.05:
                 moveRobot(0.25, -3.5, 1.5)
 
         # Comment from Saba:
@@ -175,10 +151,9 @@ class Arbiter:
         # controler we do n$
 
     def go(self):
-        output = Twist2DStamped()
-        output.v = self.speed * self.vMultiplier
-        output.omega = self.heading
-        self.pub.publish(output)
+        v = self.speed * self.vMultiplier
+        omega = self.heading
+        self.publish(v, omega)
 
     def arbitration(self):
         if not self.blocking:
